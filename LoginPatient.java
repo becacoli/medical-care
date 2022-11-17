f60af9049f4cb404783a5f2e5d0a5031514f0d1e
wemillymaria <wemillymaria40@gmail.com>
2022-11-17 18:29:11 -0300

    adding LoginPatient

---
+++ LoginPatient.java
@@ -0,0 +1,16 @@
+public class LoginPatient implements Login {
+
+//na minha cabeça isso é strategy e template method 
+
+  @Override
+  public Credentials login() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public String CreateAcc(){
+    return null;
+    
+  }
+  
+}
