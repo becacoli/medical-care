ddcd9f650701970f4012141f54f465f7dfba2628
wemillymaria <wemillymaria40@gmail.com>
2022-11-17 18:23:09 -0300

    adding Credentials

---
+++ Credentials.java
@@ -0,0 +1,11 @@
+public class Credentials {
+  String email;
+  String password;
+
+  // credenciais do doctor
+  public Credentials(String email, String password) {
+    this.email = email;
+    this.password = password;
+  }
+
+}
