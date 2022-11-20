3417ec93c0cc1789b5f8fbbdb09f0abc733b3554
Rebeca Coli <rebeca.correia100@gmail.com>
2022-11-19 21:31:48 -0300

    refactor: refactoring credentials class to register patients

--- Credentials.java
+++ Credentials.java
@@ -1,11 +1,22 @@
 public class Credentials {
-  String email;
-  String password;
+    String email;
+    String password;
 
-  // credenciais do doctor
+    String name;
+    String birthDate;
+
+  public Credentials(String name, String email,  String birthDate, String password) {
+      this.email = email;
+      this.password = password;
+      this.name = name;
+      this.birthDate = birthDate;
+    }
+
+  // credenciais do doctor/patient
   public Credentials(String email, String password) {
     this.email = email;
     this.password = password;
   }
 
+  
 }
