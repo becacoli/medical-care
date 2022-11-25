8d4e109366cfdedb0498ad7e3d633e91ddd5dc63
beqa <rebeca.correia100@gmail.com>
2022-11-25 20:53:42 -0300

    changes on main.java

--- CreditCard.java
+++ CreditCard.java
@@ -1,5 +1,8 @@
 public class CreditCard extends PaymentMethod{
 
+  public CreditCard() {
+  }
+
   @Override
   public void pay() {
     System.out.println("Pagamento com cartão de crédito feito com sucesso.");
